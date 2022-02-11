1c679d01f7f5b2f8b43d1b56a8e31c87e0d0043b
uzayaltiner <92029942+uzayaltiner@users.noreply.github.com>
2022-02-11 15:37:55 +0300

    Initial Commit

---
+++ Project27/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by Uzay Altıner on 9.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func redrawTapped(_ sender: Any) {
+    }
+    
+}
+
