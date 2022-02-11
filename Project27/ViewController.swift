2d04077baa9938f162b7b7f2c45806c3c53aed2a
uzayaltiner <92029942+uzayaltiner@users.noreply.github.com>
2022-02-11 15:54:31 +0300

    adding shaping functions

--- Project27/ViewController.swift
+++ Project27/ViewController.swift
@@ -9,14 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
+    var currentDrawType = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        drawRectangle()
+        
     }
 
     @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+        
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        case 1:
+            drawCircle()
+        default:
+            break
+        }
+    }
+    func drawCircle () {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            let rectangle = CGRect(x: 5, y: 5, width: 502, height: 502)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addEllipse(in: rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+            
+        }
+        imageView.image = image
+        
     }
     
+    func drawRectangle () {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+            
+        }
+        imageView.image = image
+    }
 }
 
